29624f9af00c281d0fd6ed8d5bd190e651347e78
davidluca3000 <davidluca3000@yahoo.com>
2016-10-27 23:32:51 +0100

    WIP: get timer task running

--- controlbox/nxp_lpcxpresso_11u14_usblib_device/Debug/Drivers/USB/Core/HAL/LPC11UXX/subdir.mk
+++ controlbox/nxp_lpcxpresso_11u14_usblib_device/Debug/Drivers/USB/Core/HAL/LPC11UXX/subdir.mk
@@ -17,7 +17,7 @@ C_DEPS += \
 Drivers/USB/Core/HAL/LPC11UXX/%.o: ../Drivers/USB/Core/HAL/LPC11UXX/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: MCU C Compiler'
-	arm-none-eabi-gcc -std=gnu99 -D__REDLIB__ -DDEBUG -D__CODE_RED -DCORE_M0 -D__LPC11U1X__ -DUSB_DEVICE_ONLY -I"M:\OneDrive\lpc11u14joystick\controlbox\lpc_chip_11uxx_lib\inc" -I"M:\OneDrive\lpc11u14joystick\controlbox\nxp_lpcxpresso_11u14_board_lib\inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-none-eabi-gcc -std=gnu99 -D__REDLIB__ -DDEBUG -D__CODE_RED -DCORE_M0 -D__LPC11U1X__ -DUSB_DEVICE_ONLY -I"C:\Users\david_000\Documents\LPCXpresso_7.6.2_326\lpc11u14joystick\controlbox\lpc_chip_11uxx_lib\inc" -I"C:\Users\david_000\Documents\LPCXpresso_7.6.2_326\lpc11u14joystick\controlbox\nxp_lpcxpresso_11u14_board_lib\inc" -O0 -g3 -Wall -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -mcpu=cortex-m0 -mthumb -D__REDLIB__ -specs=redlib.specs -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.o)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
